7a620eb613443fa19b5d42979a294a8f020ca0c5
Cthulian <julianselody@gmail.com>
2023-10-25 11:46:35 -0400

    initial commit 2

---
+++ src/interfaces/thing.ts
@@ -0,0 +1,12 @@
+export default interface Thing {
+  id: number
+  attributes: {
+    title: string
+    description: string
+    interest: string
+    createdAt: string
+    updatedAt: string
+    publishedAt: string
+    backgroundColor: string
+  }
+}
